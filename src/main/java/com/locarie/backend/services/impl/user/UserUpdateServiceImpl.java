3991fc7f4b52639b1e44551a9db53d43c8f0fd79
iltyty <qiuyh0924@163.com>
2024-01-13 11:18:56 +0800

    fix: error `A collection with cascade ="all-delete-orphan" was no longer referenced by the owning entity` when updating user

--- src/main/java/com/locarie/backend/services/impl/user/UserUpdateServiceImpl.java
+++ src/main/java/com/locarie/backend/services/impl/user/UserUpdateServiceImpl.java
@@ -38,11 +38,9 @@ public class UserUpdateServiceImpl implements UserUpdateService {
     if (optionalUserEntity.isEmpty()) {
       return new EmptyUserDto();
     }
-    UserEntity userEntity = optionalUserEntity.get();
-    deleteBusinessHoursIfDtoContainsNewOnes(userEntity, dto);
-    updateUserEntityAccordingToUpdateDto(userEntity, dto);
-    UserEntity updatedUserEntity = updateUserEntity(userEntity);
-    updateBusinessHoursIfNeeded(updatedUserEntity);
+    UserEntity oldUserEntity = optionalUserEntity.get();
+    UserEntity newUserEntity = userUpdateDtoToEntity(dto);
+    UserEntity updatedUserEntity = updateUserEntity(oldUserEntity, newUserEntity);
     return userEntityToUserDto(updatedUserEntity);
   }
 
@@ -50,14 +48,42 @@ public class UserUpdateServiceImpl implements UserUpdateService {
     return userRepository.findById(id);
   }
 
-  private void updateUserEntityAccordingToUpdateDto(UserEntity userEntity, UserUpdateDto dto) {
-    UserEntity updateUserEntity = userUpdateDtoMapper.mapFrom(dto);
-    for (Field field : updateUserEntity.getClass().getDeclaredFields()) {
+  private UserEntity userUpdateDtoToEntity(UserUpdateDto dto) {
+    return userUpdateDtoMapper.mapFrom(dto);
+  }
+
+  private UserEntity updateUserEntity(UserEntity oldEntity, UserEntity newEntity) {
+    updateBusinessHoursIfNeeded(oldEntity, newEntity);
+    updateOtherFields(oldEntity, newEntity);
+    return saveUserEntity(oldEntity);
+  }
+
+  private void updateBusinessHoursIfNeeded(UserEntity oldEntity, UserEntity newEntity) {
+    if (oldEntity.getType() == UserEntity.Type.PLAIN) {
+      return;
+    }
+    if (oldEntity.getBusinessHours() == null) {
+      oldEntity.setBusinessHours(newEntity.getBusinessHours());
+    } else {
+      businessHoursRepository.deleteAll(oldEntity.getBusinessHours());
+      oldEntity.getBusinessHours().clear();
+      oldEntity.getBusinessHours().addAll(newEntity.getBusinessHours());
+      for (BusinessHoursEntity businessHoursEntity : oldEntity.getBusinessHours()) {
+        businessHoursEntity.setUser(oldEntity);
+      }
+    }
+  }
+
+  private void updateOtherFields(UserEntity oldEntity, UserEntity newEntity) {
+    for (Field field : newEntity.getClass().getDeclaredFields()) {
+      if (field.getName().equals("businessHours")) {
+        continue;
+      }
       field.setAccessible(true);
       try {
-        Object value = field.get(updateUserEntity);
+        Object value = field.get(newEntity);
         if (value != null) {
-          field.set(userEntity, value);
+          field.set(oldEntity, value);
         }
       } catch (IllegalAccessException e) {
         throw new RequestArgumentNotValidException(e.getMessage());
@@ -65,27 +91,8 @@ public class UserUpdateServiceImpl implements UserUpdateService {
     }
   }
 
-  private void deleteBusinessHoursIfDtoContainsNewOnes(UserEntity userEntity, UserUpdateDto dto) {
-    if (userEntity.getType() == UserEntity.Type.PLAIN) {
-      return;
-    }
-    if (dto.getBusinessHours() != null && userEntity.getBusinessHours() != null) {
-      businessHoursRepository.deleteAll(userEntity.getBusinessHours());
-    }
-  }
-
-  private UserEntity updateUserEntity(UserEntity entity) {
-    return userRepository.save(entity);
-  }
-
-  private void updateBusinessHoursIfNeeded(UserEntity userEntity) {
-    if (userEntity.getType() == UserEntity.Type.PLAIN) {
-      return;
-    }
-    for (BusinessHoursEntity businessHoursEntity : userEntity.getBusinessHours()) {
-      businessHoursEntity.setUser(userEntity);
-    }
-    businessHoursRepository.saveAll(userEntity.getBusinessHours());
+  private UserEntity saveUserEntity(UserEntity userEntity) {
+    return userRepository.save(userEntity);
   }
 
   private UserDto userEntityToUserDto(UserEntity entity) {
