856e04aaf230eae93901325f7473c8a324283694
iltyty <qiuyh0924@163.com>
2024-02-27 16:33:07 +0800

    fix: post creation timestamp error

--- src/main/java/com/locarie/backend/domain/entities/PostEntity.java
+++ src/main/java/com/locarie/backend/domain/entities/PostEntity.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.generator.EventType;
 
@@ -30,7 +31,7 @@ public class PostEntity {
   @ManyToMany private List<UserEntity> favoredBy;
 
   @Column(columnDefinition = "timestamp")
-  @CurrentTimestamp(event = EventType.INSERT)
+  @CreationTimestamp
   private Instant time; // post publish time
 
   @Column(nullable = false)
