460d717bdd1b244af1c906dd95241ed026ff9319
iltyty <qiuyh0924@163.com>
2024-06-21 16:58:01 +0800

    feat: send email after registration & change email sender to contactus@locarie.com

--- src/main/java/com/locarie/backend/services/user/impl/UserRegisterServiceImpl.java
+++ src/main/java/com/locarie/backend/services/user/impl/UserRegisterServiceImpl.java
@@ -5,6 +5,7 @@ import com.locarie.backend.domain.dto.user.UserRegistrationDto;
 import com.locarie.backend.domain.entities.UserEntity;
 import com.locarie.backend.mapper.Mapper;
 import com.locarie.backend.repositories.user.UserRepository;
+import com.locarie.backend.services.mail.MailService;
 import com.locarie.backend.services.user.UserRegisterService;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,12 @@ import org.springframework.stereotype.Service;
 public class UserRegisterServiceImpl implements UserRegisterService {
   private final UserRepository repository;
   private final Mapper<UserEntity, UserDto> mapper;
+  private final MailService emailService;
 
-  public UserRegisterServiceImpl(UserRepository repository, Mapper<UserEntity, UserDto> mapper) {
+  public UserRegisterServiceImpl(UserRepository repository, Mapper<UserEntity, UserDto> mapper, MailService emailService) {
     this.repository = repository;
     this.mapper = mapper;
+    this.emailService = emailService;
   }
 
   @Override
@@ -25,6 +28,11 @@ public class UserRegisterServiceImpl implements UserRegisterService {
     }
     UserEntity user = mapper.mapFrom(dto);
     UserEntity savedUser = repository.save(user);
+    sendRegistrationSuccessEmail(user.getEmail());
     return mapper.mapTo(savedUser);
+  }
+
+  private void sendRegistrationSuccessEmail(String email) {
+    emailService.sendMail(email, "Registration Success", "Welcome to Locarie!");
   }
 }
