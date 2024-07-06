60782e4e90858e095578308725178e4a8c88b44f
iltyty <qiuyh0924@163.com>
2024-07-06 11:15:44 +0800

    feat: log error in GlobalExceptionHandler

--- src/main/java/com/locarie/backend/global/GlobalExceptionHandler.java
+++ src/main/java/com/locarie/backend/global/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import com.locarie.backend.domain.dto.ResponseDto;
 import com.locarie.backend.exceptions.*;
 import java.util.HashMap;
 import java.util.Map;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Log4j2
 public class GlobalExceptionHandler {
 
   private final ObjectMapper objectMapper;
@@ -25,6 +28,7 @@ public class GlobalExceptionHandler {
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseDto<String> handleValidationException(MethodArgumentNotValidException e)
       throws JsonProcessingException {
+    log.error(e.getMessage());
     Map<String, String> errors = new HashMap<>();
     e.getBindingResult()
         .getFieldErrors()
@@ -36,6 +40,7 @@ public class GlobalExceptionHandler {
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseDto<String> handleRequestArgumentNotValidException(
       RequestArgumentNotValidException e) {
+    log.error(e.getMessage());
     return ResponseDto.fail(ResultCode.RC101, e.getMessage());
   }
 
@@ -43,26 +48,31 @@ public class GlobalExceptionHandler {
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
   public ResponseDto<String> handleNotAuthorizedOperationException(
       NotAuthorizedOperationException e) {
+    log.error(e.getMessage());
     return ResponseDto.fail(ResultCode.RC203, e.getMessage());
   }
 
   @ExceptionHandler(UserAlreadyExistsException.class)
   public ResponseDto<String> handleUserAlreadyExistsException(UserAlreadyExistsException e) {
+    log.error(e.getMessage());
     return ResponseDto.fail(ResultCode.RC201);
   }
 
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseDto<String> handleUserNotFoundException(UserNotFoundException e) {
+    log.error(e.getMessage());
     return ResponseDto.fail(ResultCode.RC301);
   }
 
   @ExceptionHandler(PostNotFoundException.class)
   public ResponseDto<String> handlePostNotFoundException(PostNotFoundException e) {
+    log.error(e.getMessage());
     return ResponseDto.fail(ResultCode.RC401);
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseDto<String> handleException(Exception e) {
+    log.error(e.getMessage());
     return ResponseDto.fail(ResultCode.FAIL, e.getMessage());
   }
 }
